c6c94fad3bcff6c523dedd3c6a3369466bcec81b
TuongPK <125136900+trungka982004@users.noreply.github.com>
2023-05-26 00:02:16 +0700

    Add more 2 file

--- ConvenientCard.java
+++ ConvenientCard.java
@@ -1,26 +1,24 @@
-public class ConvenientCard implements Payment extends IDCard{
+public class ConvenientCard implements Payment{ //the tien loi implement Payment
 	// code here
-
+	
+    private String type;
+	private IDCard theDinhDanh;
+	private double soDuTK;
+	
 	public String getType() {
+		return this.type;
 	}
-	public ConvenientCard(IDCard theDinhDanh) throws CannotCreateCard{
-		if(theDinhDanh ==null){
-			throw new CannotCreateCard("Invalid IDCard");
-		}
-		
-		this.theDinhDanh = theDinhDanh;
-		this.soDuTK = 100;
-	}
-	public void deposit(double amount){
+	
+	public void deposit(double amount){ //tien nap
 		soDuTK += amount;
 	}
 
 	@Override
-	public double checkBalance(){
+	public double checkBalance(){ //tra ve so du 
 		return  this.soDuTK;
 	}
 	@Override
-	public boolean pay(double amount){
+	public boolean pay(double amount){ // ham kiem tra co thanh toan thanh cong hay ko 
 		double soTienThanhToan = 0.0;
 		if(type.equals("Student")){
 			soTienThanhToan = amount;
@@ -28,10 +26,6 @@ public class ConvenientCard implements Payment extends IDCard{
 				soDuTK -=soTienThanhToan;
 				return true;
 			}
-			// else{
-			// 	return false;
-			// }
-
 		}
 		else if(type.equals("Adult")) {
 			soTienThanhToan = amount + 0.01*amount;
@@ -39,14 +33,25 @@ public class ConvenientCard implements Payment extends IDCard{
 				soDuTK -=soTienThanhToan;
 				return true;
 			}
-			// else{
-			// 	return false;
-			// }
 		}
 		return false;
 	}
+
+	public ConvenientCard(IDCard theDinhDanh) throws CannotCreateCard{ //ham xac dinh loai the dua tren so tuoi
+		String date = theDinhDanh.getDayOfBirth();
+		String year = date.substring(6, 10);
+		int age = 2023 - Integer.parseInt(year);
+		if(age < 12){
+			throw new CannotCreateCard("Not enough age");
+		} else if (age >=12 && age<=18){
+			this.type = "Student";
+		} else {
+			this.type = "Adult";
+		}
+	}
+
 	@Override
-	public String toString(){
+	public String toString(){ // ham in theo format
 		return theDinhDanh.toString() + ", " + type + ", " +  soDuTK;
 	}
 }
--- IDCard.java
+++ IDCard.java
@@ -1,10 +1,25 @@
-public class IDCard {
+public class IDCard { //The dinh danh
       // code here
+      protected int soDinhDanh;
+      protected String hoTen;
+      protected String sex;
+      protected String dayOfBirth;
+      protected String address;
+      protected int phoneNumber;
 
-
-      public IDCard(int soDinhDanh,String hoTen,String sex,String dayOfBirth,String address,int phoneNumber){
-
+      public IDCard(int soDinhDanh,String hoTen,String sex,String dayOfBirth,String address,int phoneNumber){ //khoi tao day constructor du 
+            this.soDinhDanh = soDinhDanh;
+            this.hoTen = hoTen;
+            this.sex = sex;
+            this.dayOfBirth = dayOfBirth;
+            this.address = address;
+            this.phoneNumber = phoneNumber;
       }
-      public String toString(){
+      public String getDayOfBirth(){ // lay ngay thang nam
+            return dayOfBirth;
+      }
+      
+      public String toString(){ //in theo format
+            return soDinhDanh + ", " + hoTen + ", " + sex + ", " + dayOfBirth + ", " + address + ", " + phoneNumber;
       }
 }
